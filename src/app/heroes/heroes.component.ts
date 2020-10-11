9fed71ece07d6bd27ff5c44b6b93cd5165657a82
YossiBenZaken <yossibz@gmail.com>
2020-10-11 08:41:48 +0300

    .

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -82,4 +82,50 @@ export class HeroesComponent implements OnInit {
   details(hero: number): void {
     this._router.navigateByUrl(`detail/${hero}`);
   }
+  dragStart(evt:any): void {
+    evt.target.dataset.x = Number(evt.pageX) + Number(evt.target.dataset.move || 0);
+   // console.log(evt)
+  }
+  drag(evt:any) {
+    let moveX = Number(evt.target.dataset.x) - evt.pageX;
+    if (moveX > 130) {
+      moveX = 130;
+    }
+    if (moveX < -130) {
+      moveX = -130;
+    }
+    evt.target.dataset.move = moveX;
+    anime({
+      targets: evt.target,
+      translateX: -Number(evt.target.dataset.move),
+      duration: 300,
+    });
+  }
+  dragEnd(evt:any) {
+    if (evt.target.dataset.move > 100) {
+      evt.target.dataset.move = -100;
+    } else if (evt.target.dataset.move < -100) {
+      evt.target.dataset.move = 100;
+    } else {
+      evt.target.dataset.move = 0;
+    }
+    const swipes = document.querySelectorAll('.swipe') || [];
+    swipes.forEach((item) => {
+      if (item.querySelector('.con-text') === evt.target) {
+        return;
+      }
+      item.querySelector('.con-text').dataset.move = 0;
+      item.querySelector('.con-text').dataset.x = 0;
+      anime({
+        targets: item.querySelector('.con-text'),
+        translateX: 0,
+      });
+    });
+    setTimeout(() => {
+      anime({
+        targets: evt.target,
+        translateX: -Number(evt.target.dataset.move),
+      });
+    }, 1);
+  }
 }
